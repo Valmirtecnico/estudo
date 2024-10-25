9cb7ba694a9035091cc7544df566edb398cf1acd
Valmirtecnico <valmirtecnicoespecialista@gmail.com>
2024-10-25 00:24:58 +0000

    exercicios1-3

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-let n1 = Number(prompt('Digite n1'));
-let n2 = Number(prompt('Digite n2'));
-let media = (n1 + n2) / 2;
-alert('Média:' + media.toFixed(2));
+//exemplo 1
+// let n1 = Number(prompt('Digite n1'));
+// let n2 = Number(prompt('Digite n2'));
+// let media = (n1 + n2) / 2;
+// alert('Média:' + media.toFixed(2));
 
-if (media < 5) {
-  alert('Reprovado');
-} else {
-  alert('Aprovado');
-}
+// if (media < 5) {
+//   alert('Reprovado');
+// } else {
+//   alert('Aprovado');
+// }
+
+
+// exercicio 1
+// let number = Number(prompt("Digite um numero:"))
+// alert(`${number+1} e ${number-1}`)
+
+// exercicio 2
+// let totalBill = Number(prompt("Qual o valor R$?"))
+// let howManyPeople = parseInt(prompt("Quantas pessoas?"))
+// let priceForEachPerson = totalBill/ howManyPeople
+// alert(`R$ ${priceForEachPerson.toFixed(2)}`)
+
+// exercicio 3
+// let price = parseFloat(prompt("Qual o preço?"))
+// alert(`A Vista R$ ${(price-price*0.1).toFixed(2)} ou 3x de R$ ${(price/3).toFixed(2)}`)
